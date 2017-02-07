8389001b318d89adc57aa31ff7aacf7a66c2244d
TSturtledove <roberttortorelli@yahoo.com>
2017-02-07 08:42:48 -0600

    initialized Firebase

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
+var config = {
+    apiKey: "AIzaSyDbwkH02G26zGUloMdZZhqyKyuMvZN2EsA",
+    authDomain: "frontend-760f7.firebaseapp.com",
+    databaseURL: "https://frontend-760f7.firebaseio.com",
+    storageBucket: "frontend-760f7.appspot.com",
+    messagingSenderId: "372869057554"
+  };
+  firebase.initializeApp(config);
+
+
+
+
 const app = angular.module("storyApp", ["ngRoute"])
 app.config(function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix("");
